330c1d97df6edc6fb8c9c02ac288b606c3075e59
Juan Carlos <jccastillo.brito@gmail.com>
2021-02-19 18:31:11 -0300

    codigo en R analisis basico Text Mining

--- testR.R
+++ testR.R
@@ -1,28 +1,132 @@
 library(quanteda)
 library(readtext)
 library(quanteda.textplots)
-
-
-dat_csv <- read.csv(file = 'gonzalo_text.csv', header = TRUE,sep=';')
-
-corpus_test = corpus(dat_csv$tweet)
-
-dfm_inaug <- corpus_subset(corpus_test) %>% 
-  dfm(remove = stopwords('spanish'), remove_punct = TRUE) %>%
-  dfm_trim(min_termfreq = 10, verbose = FALSE)
+library(quanteda.textmodels)
+library(stringr)
+library(topicmodels)
+library(tidyverse)
+library(tidytext)
+library(NLP)
+library(tm)
+library(wordcloud)
+library(cluster)
 
 set.seed(100)
-textplot_wordcloud(dfm_inaug)
+#filecsv = 'penaloza_text.csv'
+#filecsv = 'kast_text.csv'
+#filecsv = 'daniel_text.csv'
+#filecsv = 'lavin_text.csv'
+filecsv = 'gonzalo_text.csv'
 
-user_dfm <- dfm_select(dfm_inaug, pattern = "@*")
-topuser <- names(topfeatures(user_dfm, 50))
-head(topuser)
+name = 'Gonzalo de la Carrera'
+#name = 'Daniela Peñaloza'
+#name = 'Daniel Jadue'
+#name  = 'Kast'
+#name  = 'Lavin'
 
-user_fcm <- fcm(user_dfm)
+#setear carpeta con archivos
+setwd("C:/Repositorios/Otros/textMiningPython")
+
+# stpwords español
+stop <- stopwords('spanish')
+
+## basura ajena a las stopwors
+eliminar <-c('gonzalo','c','rt','xd','da','si','cmo','q','t','3',
+                      'p','s','si','d','m','ser','mas','carrera','así','asi',
+                      'bien','sr','x','de','la','a','el','condes','ja','las','hoy')
+
+stop <- c(stop,eliminar)
+
+#Extraccion de data
+dat_csv <- read.csv(file = filecsv , header = TRUE,sep=';', encoding = "UTF-8")
 
 
-user_fcm <- fcm_select(user_fcm, pattern = topuser)
-textplot_network(user_fcm, min_freq = 0.1, edge_color = "orange", edge_alpha = 0.8, edge_size = 5)
+#minusculas
+clean <-  data.frame("text"=tolower(dat_csv$tweet))
 
-textplot_wordcloud(dfm_inaug, min_count = 10,
-                   color = c('red', 'pink', 'green', 'purple', 'orange', 'blue'))
+# stopword
+clean$text <- removeWords(clean$text, words = stop)
+
+#eliminacion de url
+clean <- data.frame("text"=gsub(" ?(f|ht)tp(s?)://(.*)[.][a-z]+", "", clean$text))
+
+#Eliminar usuarios
+clean$text <-gsub("@([A-Za-z0-9_]{1,15})", "", clean$text)
+
+#Eliminar hasgtag
+clean$text <-gsub("#([A-Za-z0-9_]{1,15})", "", clean$text)
+
+#Eliminar ../XXX
+clean$text  <- gsub('(...)?/(.*)', "", clean$text)
+
+#Solo alfanumericos
+clean$text  <- gsub('[^A-Za-zÀ-ÿ ]', "", clean$text)
+
+#mas de un espacio
+clean$text  <- gsub('\\s{2}(\\s*)?', " ", clean$text)
+
+# espacios inicio y fin
+clean$text <- str_trim(clean$text)
+
+# espacios en blanco excesivos
+clean$text <- stripWhitespace(clean$text)
+
+
+#Limpieza valores vacios y NA
+clean <- clean[clean$text != '', ]
+clean <- clean[!is.na(clean$text),]
+
+#Asignacion nuevos valores 
+clean <-data.frame('text'=clean)
+
+n_grams = 2
+repeticiones <- clean %>%
+                unnest_tokens(bigrama,
+                text,
+                token = "ngrams",
+                n = n_grams)
+
+
+repeticiones %>%
+  filter(!is.na(bigrama)) %>%
+  count(bigrama, sort = T) %>%
+  top_n(10) %>%
+  ggplot() +
+  geom_col(aes(y = n , x = reorder(bigrama,n)),
+           fill = "maroon") +
+  coord_flip() +
+  theme_linedraw() + 
+  labs(x = "Bigramas", y = "Frecuencia") + 
+  ggtitle("Trigramas más frecuentes ", subtitle = name)
+
+
+## Relaciones
+corpus <- VCorpus(VectorSource(clean$text))
+
+## mapeo de corpus
+nov_ptd <- tm_map(corpus, PlainTextDocument)
+
+#nube de palabras
+wordcloud(nov_ptd,min.freq = 3, max.words = 300, random.order = F, colors = brewer.pal(name = "Dark2", n = 8))
+
+#matriz de terminos
+nov_tdm <- TermDocumentMatrix(nov_ptd)
+nov_mat <- as.matrix(nov_tdm)
+
+
+##se quitan palabras dispersas,definir  sparse alto significa valores comunes
+nov_new <- removeSparseTerms(nov_tdm, sparse = .98)
+
+nov_dist <- dist(nov_new, method = "euclidian")
+
+nov_hclust <-  hclust(nov_dist, method = "complete")
+
+plot(nov_hclust, main = paste("Dendrograma",name,sep=" "), sub = "", xlab = "")
+
+# K es indefinido automaticamente
+rect.hclust(nov_hclust, k = 4, border="blue")
+
+#dendograma ordenado
+nov_agnes <- agnes(nov_dist, method = "average")
+plot(nov_agnes, which.plots = 2, main = paste("Dendrograma",name,sep=" "), sub = "", xlab = "")
+
--- test_twitter.py
+++ test_twitter.py
@@ -6,7 +6,6 @@ Created on Fri Sep  4 20:45:41 2020
 """
 # -*- coding: utf-8 -*-
 import tweepy
-import csv
 import pandas as pd
 
 
@@ -20,10 +19,6 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
-# Open/Create a file to append data
-csvFile = open('gonzalo.csv', 'w',encoding="utf-8")
-#Use csv Writer
-csvWriter = csv.writer(csvFile,delimiter=';')
 
 cont = 0
 cant_item= 100
@@ -31,13 +26,23 @@ cols = ['fecha','autor','tweet']
 
 lst = []
 
+# @danielapenaloza
+# @carreragonzalo
+# @danieljadue
+# @joseantoniokast
 
-for tweet in tweepy.Cursor(api.search,q="@carreragonzalo",
+#%% call api
+print('comienzo')
+for tweet in tweepy.Cursor(api.search,q="@joseantoniokast -filter:retweets",
                            exclude_replies=True,
-                           until ="2021-02-18").items():         
+                           exclude_retweets=True,
+                           until ="2021-02-19").items():         
     
     lst.append([tweet.created_at,tweet.user.screen_name,tweet.text])
     cont += 1
+    print(cont)
+    if cont == 2699:
+        break
 
 df = pd.DataFrame(data=lst,columns = cols)
-df.to_csv('gonzalo_text.csv',index=False,sep=';')
+df.to_csv('kast_text.csv',index=False,sep=';')
